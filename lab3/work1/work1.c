d3b94301b79744e226b1696941566bb77aff807f
SolitaryM <zzzxxx804113401@gmail.com>
2020-04-14 19:25:03 +0800

    Add lab3 work1

---
+++ lab3/work1/work1.c
@@ -0,0 +1,16 @@
+#include <linux/kernel.h>
+#include <asm/current.h>
+/*
+#define SCHED_NORMAL            0
+#define SCHED_FIFO              1
+#define SCHED_RR                2
+#define SCHED_BATCH             3
+// SCHED_ISO: reserved but not implemented yet 
+#define SCHED_IDLE              5
+#define SCHED_DEADLINE          6
+*/
+asmlinkage long work1(void){
+	printk("pid is %d\n",current->pid);
+	char policy[7][20]{"SCHED_NORMAL","SCHED_FIFO","SCHED_BATCH","reserved","SCHED_IDLE","SCHED_DEADLINE"};
+	printk("schedule policy is %s",policy[current->policy]);
+}
